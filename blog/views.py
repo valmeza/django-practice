079240b5873bf5fdd0cdc067730f8551578b39b5
Valeria Meza <valeriameza173@gmail.com>
2020-11-12 15:36:13 -0600

    add migrations and register admin user

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'Valeria Meza',
-        'title': 'Some Blog',
-        'content': 'My first post',
-        'date_posted': 'November 11, 2020'
-    },
-    {
-        'author': 'Valeria Something',
-        'title': 'Some Other Blog',
-        'content': 'This post',
-        'date_posted': 'November 1, 2019'
-    }
-    ,
-    {
-        'author': 'Valeria Something',
-        'title': 'Some Other Blog',
-        'content': 'This post',
-        'date_posted': 'November 1, 2019'
-    }
-    ,
-    {
-        'author': 'Valeria Something',
-        'title': 'Some Other Blog',
-        'content': 'This post',
-        'date_posted': 'November 1, 2019'
-    }
-]
 
 def home(request):
     data = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, 'blog/home.html', data)
