e99051a7cbb8ff7786abd381a9bf3ef301b0fa33
Valeria Meza <valeriameza173@gmail.com>
2020-11-14 14:56:07 -0600

    fix issue where post request was not redirecting to the correct view.

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        form.is_valid()
+        username = form.cleaned_data.get('username')
+        messages.success(request, f'Successfully created {username}')
+
+        return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
